7b795f8cc7a684ff6df4c31ab2fe823ef831a2ac
Marcos Soares <marcossaore@gmail.com>
2021-05-20 08:08:15 -0300

    test: add name to autentication response

--- src/presentation/controllers/login/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login/login-controller.spec.ts
@@ -38,9 +38,9 @@ describe('Login Controller', () => {
     })
   })
 
-  test('Should return 401 if invalid credentials are provided', async () => {
+  test('Should return 401 if invalid AutenticationModel are provided', async () => {
     const { sut, authenticationSpy } = makeSut()
-    authenticationSpy.token = null
+    authenticationSpy.autenticationModel = null
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(unauthorized())
   })
@@ -52,10 +52,10 @@ describe('Login Controller', () => {
     expect(httpResponse).toEqual(serverError(new Error()))
   })
 
-  test('Should return 200 if valid credentials are provided', async () => {
+  test('Should return 200 if valid AutenticationModel are provided', async () => {
     const { sut, authenticationSpy } = makeSut()
     const httpResponse = await sut.handle(mockRequest())
-    expect(httpResponse).toEqual(ok({ accessToken: authenticationSpy.token }))
+    expect(httpResponse).toEqual(ok(authenticationSpy.autenticationModel))
   })
 
   test('Should call Validation with correct value', async () => {
